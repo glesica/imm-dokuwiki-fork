ae4057dc12052f0891885dd2b9fcea2ff0eeff58
George Lesica <glesica@gmail.com>
2010-08-18 01:11:50 -0500

    Updated copyright notice.

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <div class="footer">
 	<div class="pageinfo"><?php tpl_pageinfo(); ?></div>
 	<div class="actions icon_buttons"><?php tpl_button('edit'); tpl_button('index'); tpl_button('admin'); ?></div>
-	<div class="copyright">Copyright 2010 <a href="mailto:webmaster@ianmcintosh.org">Ian McIntosh</a></div>
+	<div class="copyright">Theme: <a href="http://github.com/glesica/imm-dokuwiki-fork">imm-dokuwiki-fork</a>, original theme copyright 2010 <a href="mailto:webmaster@ianmcintosh.org">Ian McIntosh</a></div>
 	<div class="copyright"><a href="http://www.famfamfam.com/lab/icons/silk/">Silk</a> Icon Set</div>
 </div>
