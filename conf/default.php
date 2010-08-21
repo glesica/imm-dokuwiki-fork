b5f4a77c2c3a51a8a6e761f3ab3f3e925c637ae6
George Lesica <glesica@gmail.com>
2010-08-21 02:50:33 -0500

    Added conf. Menu and sidebar page name options added.

---
+++ conf/default.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Default configuration for the imm-dokuwiki-fork template
+ * 
+ * @author: George Lesica <glesica@gmail.com>
+ */
+
+$conf['menuname']                    = 'menu';                       // name of menu pages
+$conf['sidebarname']                 = 'sidebar';                    // name of sidebar pages
+?>
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * configuration-manager metadata for the imm-dokuwiki-fork
+ * 
+ * @author: George Lesica <glesica@gmail.com>
+ */
+
+$meta['menuname']                 = array('string', '_pattern' => '#[a-z0-9]*#');
+$meta['sidebarname']              = array('string', '_pattern' => '#[a-z0-9]*#');
+?>
