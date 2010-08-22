55f32560cf874287fa6b75ed6484df13c9a574dd
George Lesica <glesica@gmail.com>
2010-08-22 01:58:08 -0500

    Added en translation strings for config options.

---
+++ lang/en/settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * English translation strings for the imm-dokuwiki-fork
+ * 
+ * @author: George Lesica <glesica@gmail.com>
+ */
+ 
+$lang['usesidebar']                  = 'Display a sidebar';
+$lang['menuname']                    = 'Pagename for sidebar menu';
+$lang['sidebarname']                 = 'Pagename for sidebar content';
